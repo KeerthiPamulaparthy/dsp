e193d003444f11a6d1bb738372c643f593d4e629
KeerthiPamulaparthy <reddy.keerthi@gmail.com>
2016-12-15 09:45:21 -0600

    Completed

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,33 @@
 # Hint:  use Google to find python function
 
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+import time
+from datetime import date
+date_start = '01-02-2013'
+date_start = datetime.strptime('01-02-2013','%m-%d-%Y')
+date_stop = '07-28-2015'
+date_stop = datetime.strptime('07-28-2015','%m-%d-%Y')
+timedelta = date_stop - date_start
+timedelta.days
 
 ####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+import time
+from datetime import date
+date_start = '12312013'
+date_start = datetime.strptime('12312013','%m%d%Y')
+date_stop = '05282015'
+date_stop = datetime.strptime('05282015','%m%d%Y')
+timedelta = date_stop - date_start
+timedelta.days
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+####c)   
+import time
+from datetime import date
+date_start = '15-Jan-1994'      
+date_start = datetime.strptime('15-Jan-1994','%d-%b-%Y')
+date_start
+date_stop = '14-Jul-2015'
+date_stop = datetime.strptime('14-Jul-2015','%d-%b-%Y')
+date_stop
+timedelta = date_stop - date_start
+timedelta.days
